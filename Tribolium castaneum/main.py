3e43bdf5f29ee885015cdf0b2a9e45f57dc2c851
dvserrano <dvserrano@uninorte.edu.co>
2021-10-12 20:42:40 -0500

    cambios

--- Tribolium castaneum/main.py
+++ Tribolium castaneum/main.py
@@ -6,9 +6,38 @@ app = Flask (__name__)
 def inicio():
     return render_template('login.html')
 
-@app.route('/CrearPub', methods=['GET'])
+@app.route('/registro', methods=['POST', 'GET'])
+def registro():
+    return render_template('registro.html')
+
+@app.route('/crear', methods=['GET'])
 def crear():
     return render_template('CrearPub.html')
+
+@app.route('/buscar')
+def buscar():
+    return render_template('buscar.html')
+
+@app.route('/chat')
+def chat():
+    return render_template('chatymensajes.html')
+
+@app.route('/feed')
+def feed():
+    return render_template('feed.html')
+
+@app.route('/notificaciones')
+def notificaciones():
+    return render_template('notificaciones.html')
+
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
+
+
+
+
+
 
 
 
