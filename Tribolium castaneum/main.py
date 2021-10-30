b9010da10ad4f85d20ec1e23049af40c0f07c6c2
jfagu <91998304+jfagu@users.noreply.github.com>
2021-10-30 12:10:41 -0500

    intentando con heroku

--- Tribolium castaneum/main.py
+++ Tribolium castaneum/main.py
@@ -227,9 +227,6 @@ def feedAdminEliminar(id_publicacion):
                 lista.append(temp)
             return redirect(url_for('feedAdmin'))
     return redirect(url_for('feedAdmin'))
-
-
-
-
+    
 # if __name__ == "__main__":
 #     app.run(debug= True)
